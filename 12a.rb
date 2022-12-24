2c87f9ed4725603cbeef38fd253563869721464a
Jonas Arvidsson <jonas054@gmail.com>
2022-12-24 20:44:00 +0100

    New solution from reddit

--- 12a.rb
+++ 12a.rb
@@ -1,3 +1,6 @@
+# Algorithm stolen from
+# https://github.com/valogonor/advent-of-code/blob/main/2022/day12.py
+
 EXAMPLE = <<~TEXT.freeze
   Sabqponm
   abcryxxl
@@ -6,14 +9,9 @@ EXAMPLE = <<~TEXT.freeze
   abdefghi
 TEXT
 
-START_MARK = 'S'
-END_MARK = 'E'
-
 def main(input)
   @grid = input.lines.map(&:chomp)
-  start_pos = find_pos(START_MARK)
-  @end_pos = find_pos(END_MARK)
-  p shortest_path_from([start_pos]).length - 1
+  p breadth_first_search(find_pos('S'), find_pos('E'))
 end
 
 def find_pos(marker)
@@ -24,88 +22,35 @@ def find_pos(marker)
   end
 end
 
-def shortest_path_from(travelled)
-  output travelled
-  current_pos = travelled.last
-  return travelled if elevation(current_pos) == END_MARK
+def breadth_first_search(start_pos, end_pos)
+  seen = {}
+  queue = [[start_pos]]
+  while queue.any?
+    path = queue.shift
+    pos = path.last
+    next if seen.include?(pos)
+    return path.length - 1 if pos == end_pos
 
-  climbable = steps_inside_grid(current_pos).select do |step|
-    new_position = current_pos + step
-    current_elevation = elevation(current_pos)
-    new_elevation = elevation(new_position)
-    (new_elevation == END_MARK && current_elevation.ord >= 'y'.ord) ||
-      (new_elevation != END_MARK &&
-       (new_elevation.ord - current_elevation.ord) < 2)
+    seen[pos] = true
+    [0 - 1i, 0 + 1i, -1, 1].each do |dir|
+      new_pos = pos + dir
+      next unless (0...@grid.length).cover?(new_pos.imag) &&
+                  (0...@grid[0].length).cover?(new_pos.real)
+      next unless elevation(new_pos) - elevation(pos) < 2
+
+      queue << path + [new_pos]
+    end
   end
-  possible_next = climbable.map { current_pos + _1 } - travelled
-  paths = possible_next.map { shortest_path_from(travelled + [_1]) }.compact
-  successful = paths.select { elevation(_1.last) == END_MARK }
-
-  # output successful.min_by(&:length) if successful.min_by(&:length)
-  successful.min_by(&:length)
-end
-
-def steps_inside_grid(current_pos)
-  order = [0 + 1i, 0 - 1i, 1 + 0i, -1 + 0i].select do |step|
-    new_position = current_pos + step
-    new_position.real >= 0 && new_position.real < @grid[0].length &&
-      new_position.imag >= 0 && new_position.imag < @grid.length
-  end
-  diff = @end_pos - current_pos
-  wanted_direction = Complex(max_one(diff.real), max_one(diff.imag))
-  order.sort_by { sort_order(_1, wanted_direction, current_pos) }
-end
-
-def sort_order(dir, wanted_dir, current_pos)
-  order = if dir == wanted_dir
-            0
-          elsif dir.real == wanted_dir.real
-            1
-          elsif dir.imag == wanted_dir.imag
-            2
-          else
-            3
-          end
-
-  order = -100 if elevation(current_pos + dir) == END_MARK
-  order -= 2 if elevation(current_pos + dir) == elevation(current_pos)
-  order -= 4 if elevation(current_pos + dir) > elevation(current_pos)
-  order += 4 if elevation(current_pos + dir) < elevation(current_pos)
-  order
-end
-
-def max_one(n)
-  n == 0 ? 0 : n / n.abs
 end
 
 def elevation(position)
   letter = @grid[position.imag][position.real]
-  letter == START_MARK ? 'a' : letter
+  case letter
+  when 'S' then 'a'
+  when 'E' then 'z'
+  else letter
+  end.ord
 end
 
-print "\033[2J"
-print "\033[?25l"
-
-@output_count = 0
-INTERVAL = 20_000
-
-def output(path)
-  print "\033[0;0H"
-  return if (@output_count += 1) % INTERVAL != 0
-
-  @grid.each_with_index do |row, y|
-    row.chars.each_with_index do |value, x|
-      pos = Complex(x, y)
-      if path.include?(pos)
-        print(pos == path.last ? Rainbow(value).yellow : Rainbow(value).green)
-      else
-        print(Rainbow(value).blue)
-      end
-    end
-    puts
-  end
-  printf "%.1e\n", @output_count
-end
-
-# main(EXAMPLE)
-main(File.read('12.input'))
+main(EXAMPLE) # 31
+main(File.read('12.input')) # 472
