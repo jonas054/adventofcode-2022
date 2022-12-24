00530af64604864f54a6e38dd6f54c4118038593
Jonas Arvidsson <jonas054@gmail.com>
2022-12-24 21:05:56 +0100

    Refactor

--- 12a.rb
+++ 12a.rb
@@ -10,17 +10,15 @@ EXAMPLE = <<~TEXT.freeze
 TEXT
 
 def main(input)
-  @grid = input.lines.map(&:chomp)
+  # Map the input into a hash with positions as keys, e.g. { 0+0i => 'S', ... }
+  @grid = input.lines.each_with_index.map do |line, row_index|
+    line.chomp.chars.each_with_index.map { [Complex(_2, row_index), _1] }
+  end.flatten(1).to_h
+
   p breadth_first_search(find_pos('S'), find_pos('E'))
 end
 
-def find_pos(marker)
-  @grid.each_with_index do |row, y|
-    row.chars.each_with_index do |char, x|
-      return Complex(x, y) if char == marker
-    end
-  end
-end
+def find_pos(marker) = @grid.keys.find { @grid[_1] == marker }
 
 def breadth_first_search(start_pos, end_pos)
   seen = {}
@@ -28,29 +26,18 @@ def breadth_first_search(start_pos, end_pos)
   while queue.any?
     path = queue.shift
     pos = path.last
-    next if seen.include?(pos)
     return path.length - 1 if pos == end_pos
+    next if seen[pos]
 
     seen[pos] = true
-    [0 - 1i, 0 + 1i, -1, 1].each do |dir|
-      new_pos = pos + dir
-      next unless (0...@grid.length).cover?(new_pos.imag) &&
-                  (0...@grid[0].length).cover?(new_pos.real)
-      next unless elevation(new_pos) - elevation(pos) < 2
-
-      queue << path + [new_pos]
-    end
+    [0 - 1i, 0 + 1i, -1, 1].map { pos + _1 }
+                           .select { @grid.key?(_1) }
+                           .select { elevation(_1) - elevation(pos) < 2 }
+                           .each { queue << path + [_1] }
   end
 end
 
-def elevation(position)
-  letter = @grid[position.imag][position.real]
-  case letter
-  when 'S' then 'a'
-  when 'E' then 'z'
-  else letter
-  end.ord
-end
+def elevation(pos) = @grid[pos].tr('SE', 'az').ord
 
 main(EXAMPLE) # 31
 main(File.read('12.input')) # 472
